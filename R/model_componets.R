8cb6f453adb572f80a89427ced0a67093e628f18
kdorheim <kalyn.dorheim@pnnl.gov>
2022-07-01 13:52:52 -0400

    debugging

--- R/model_componets.R
+++ R/model_componets.R
@@ -42,32 +42,32 @@ carbon_pools <-function(t, env, state = NULL, params = NULL,
     ff <- fx[["flux_function"]]
     fluxes <- ff(env)
 
-    expected_fluxes <- rep('F', length.out = length(1:8))
-    expected_fluxes <- c(paste0(expected_fluxes, 1:8), 'F9.ep', 'F9.em', 'F10.ep', 'F10.em')
-    assertthat::assert_that(assertthat::has_name(x = fluxes, which = expected_fluxes))
+    #expected_fluxes <- rep('F', length.out = length(1:8))
+    #expected_fluxes <- c(paste0(expected_fluxes, 1:8), 'F9.ep', 'F9.em', 'F10.ep', 'F10.em')
+    #assertthat::assert_that(assertthat::has_name(x = fluxes, which = expected_fluxes))
     assertthat::assert_that(all(unlist(lapply(fluxes, is.function))), msg = 'fluxes input must be a list of functions')
 
     # Define the system of differential equations to describes the
     # changes in the carbon pool states.
     # -----------------------------------------------------------
     # P = particulate organic carbon
-    dP <- I.p + (1 - g.d) * fluxes$F8() - fluxes$F2()
+    dP <- (1 - g.d) * fluxes$F8() - fluxes$F2() + Input
     # M = mineral-associated organic carbon (MOC)
-    dM <- (1 - f.d) * fluxes$F2() - fluxes$F3()
+    dM <- (1 - f.d) * fluxes$F2() - fluxes$F3() + Input
     # Q = active layer of MOC
     dQ <- fluxes$F6() - fluxes$F7()
     # B = microbial biomass carbon
-    dB <- fluxes$F1() - (fluxes$F4() + fluxes$F5()) - fluxes$F8() - (fluxes$F9.ep() + fluxes$F9.em())
+    dB <- fluxes$F1() * CUE - fluxes$F8() - (fluxes$F9.ep() + fluxes$F9.em())
     # D = dissolved organic carbon
-    dD <- I.d + f.d * fluxes$F2() + g.d * fluxes$F8() + fluxes$F3() + (fluxes$F10.em() + fluxes$F10.ep()) - fluxes$F1() - (fluxes$F6() - fluxes$F7())
+    dD <- f.d * fluxes$F2() + g.d * fluxes$F8() + fluxes$F3() + (fluxes$F10.em() + fluxes$F10.ep()) - fluxes$F1() - (fluxes$F6() - fluxes$F7())
     # EP = carbon stored as extra-cellular enzymes
     dEP <- fluxes$F9.em() - fluxes$F10.ep()
     # EM = carbon stored as extra-cellular enzymes
     dEM <- fluxes$F9.em() - fluxes$F10.em()
     # IC = inorganic carbon (CO2)
-    dIC <- fluxes$F4() + fluxes$F5()
+    dIC <- fluxes$F1() * (1 - CUE)
     # Tot = the total carbon pool
-    dTot <- I.p + I.d - (fluxes$F4() + fluxes$F5())
+    dTot <- -fluxes$F1() * (1-CUE) + Input * 2
 
     # Return outputs
     list(c(dP, dM, dQ, dB, dD, dEP, dEM, dIC, dTot))
@@ -105,7 +105,7 @@ carbon_fluxes_internal <-function(env, state = NULL, params = NULL) {
     fxn_list <- list(
       "F1" = function() {
         # DOC uptake by microbial biomass.
-        (1 / E.c) * (V.d + m.r) * ((B * D ) / (K.d + D))
+        V.d * B * D / (K.d + D)
       },
       "F2" = function() {
         # POC decomposition
@@ -115,33 +115,33 @@ carbon_fluxes_internal <-function(env, state = NULL, params = NULL) {
         # Break down of mineralized organic carbon
         (V.m * EM * M) / (K.m + M)
       },
-      "F4" = function() {
-        # Microbial respiration from biomass growth
-        ((1 / E.c) - 1) * (V.d * B * D) / (K.d + D)
-      },
-      "F5" = function() {
-        # Metabolic/maintenance microbial respiration
-        ((1 / E.c) - 1) * (m.r * B * D) / (K.d + D)
-      },
+      # "F4" = function() {
+      #   # Microbial respiration from biomass growth
+      #   ((1 / E.c) - 1) * (V.d * B * D) / (K.d + D)
+      # },
+      # "F5" = function() {
+      #   # Metabolic/maintenance microbial respiration
+      #   ((1 / E.c) - 1) * (m.r * B * D) / (K.d + D)
+      # },
       "F6" = function() {
         # Adsorption of DOC to mineral-associated organic carbon
         K.ads * D * (1 - Q / Q.max)
       },
       "F7" = function() {
         # Desorption of mineral-associated organic carbon to DOC
-        K.des * (Q / Q.max)
+        K.des * Q / Q.max
       },
       "F8" = function() {
         # Carbon loss due to microbial biomass mortality
-        (1 - p.ep - p.em) * m.r * B
+        (1 - p.ep - p.em) * B * 0.4 * V.d * B
       },
       "F9.ep" = function() {
         # Enzyme production
-        p.ep * m.r * B
+        p.ep * B * 0.4 * V.d
       },
       "F9.em" = function() {
         # Enzyme production
-        p.em * m.r * B
+        p.em * B * 0.4 * V.d
       },
       "F10.ep" = function() {
         # Enzyme turn over
@@ -224,23 +224,18 @@ carbon_fluxes <-
           fluxes[["F1"]] = function() {
             # DOC uptake by microbial biomass.
             # Michaelis menten kinetics
-            (1 / E.c) * (V.d + m.r) * B * D / (K.d + D)
+            V.d * B * D / (K.d + D)
           }
         } else if (DOMdecomp == "RMM") {
           fluxes[["F1"]] = function() {
             # DOC uptake by microbial biomass.
             # Reverse michaelis menten kinetics
-            (1 / E.c) * (V.d + m.r) * B * D / (K.d + B)
+            V.d * B * D / (K.d + B)
           }
         } else if (DOMdecomp == "ECA") {
           fluxes[["F1"]] = function() {
             # DOC uptake by microbial biomass, note that this uses ECA kinetics.
-            (1 / E.c) * (V.d + m.r) * B * D / (K.d + B + D)
-          }
-        } else if (DOMdecomp == "LM") {
-          fluxes[["F1"]] = function() {
-            # DOC uptake by microbial biomass, note that this uses a linear model of kinetics.
-            V.d * B * D / (K.d + B)
+            V.d * B * D / (K.d + B + D)
           }
         }
 
@@ -263,11 +258,6 @@ carbon_fluxes <-
             # POC uptake by microbial biomass, note that this uses ECA kinetics.
             V.p * EP * P / (K.p + EP + B)
           }
-        } else if (POMdecomp == "LM") {
-          fluxes[["F2"]] = function() {
-            # POC uptake by microbial biomass, note that this uses a linear model of kinetics.
-            V.p * EP * P / (K.p + B)
-          }
         }
 
         # If else statement determining the kinetics used in decay of the MB.
@@ -276,14 +266,14 @@ carbon_fluxes <-
             # Density dependent
             # Carbon loss due to microbial biomass mortality.
             assert_that(dd.beta > 1)
-            (1 - p.ep - p.em) * m.r * B ^ dd.beta
+            (1 - p.ep - p.em) * 0.4 * B ^ dd.beta
           }
         } else if (MBdecay == "LM") {
           fluxes[["F8"]] = function() {
             # Linear relationship
             # Carbon loss due to microbial biomass mortality.
             assert_that(dd.beta == 1)
-            (1 - p.ep - p.em) * m.r * B ^ dd.beta
+            (1 - p.ep - p.em) * 0.4 * B ^ dd.beta
           }
         }
 
