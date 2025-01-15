53ad17bba2afb89789326657bbf4d47a052adb9d
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2025-01-15 09:06:14 -0500

    Remove memc_incubation object

--- tests/testthat/test-data.R
+++ tests/testthat/test-data.R
@@ -1,9 +1,24 @@
 test_that("incubation data", {
-  
-  expect_identical(class(memc_incubation), "list")
-  expect_equal(length(memc_incubation), 4)
-  expect_identical(class(memc_incubation$ultisol$data), "data.frame")
-  expect_identical(class(memc_incubation$ultisol$state), "numeric")
+
+    expect_is(memc_incubation_andisol, "list")
+    expect_identical(length(memc_incubation_andisol), 2L)
+    expect_is(memc_incubation_andisol$data, "data.frame")
+    expect_is(memc_incubation_andisol$state, "numeric")
+
+    expect_is(memc_incubation_gelisol, "list")
+    expect_identical(length(memc_incubation_gelisol), 2L)
+    expect_is(memc_incubation_gelisol$data, "data.frame")
+    expect_is(memc_incubation_gelisol$state, "numeric")
+
+    expect_is(memc_incubation_mollisol, "list")
+    expect_identical(length(memc_incubation_mollisol), 2L)
+    expect_is(memc_incubation_mollisol$data, "data.frame")
+    expect_is(memc_incubation_mollisol$state, "numeric")
+
+    expect_is(memc_incubation_ultisol, "list")
+    expect_identical(length(memc_incubation_ultisol), 2L)
+    expect_is(memc_incubation_ultisol$data, "data.frame")
+    expect_is(memc_incubation_ultisol$state, "numeric")
 
 })
 
